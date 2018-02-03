e5bfc980c6def40a72d4d48f805d36542993eb75
Rajan Khullar <rkhullar03@gmail.com>
2018-02-03 15:57:35 -0500

    init; added method stub

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+    public static void hello(String message, int count)
+    {
+        // your code
+    }
+
+    public static void main(String[] args)
+    {
+        hello("hello world", 4);
+    }
+}
